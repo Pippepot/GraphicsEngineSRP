d957c96357baa77ead4bce558912a7eb0be0b718
Philip Thomsen <philip@ubt.dk>
2021-03-20 10:55:01 +0100

    moved triangle clip against plane to math

--- 3DEngine/3DEngine.cpp
+++ 3DEngine/3DEngine.cpp
@@ -121,142 +121,6 @@ private:
 
 	olc::Sprite* sprTex1;
 
-	int Triangle_ClipAgainstPlane(vec3d plane_p, vec3d plane_n, triangle& in_tri, triangle& out_tri1, triangle& out_tri2)
-	{
-		// Make sure plane normal is indeed normal
-		plane_n = plane_n.Normalise();
-
-		// Return signed shortest distance from point to plane, plane normal must be normalised
-		auto dist = [&](vec3d& p)
-		{
-			vec3d n = p.Normalise();
-			return (plane_n.x * p.x + plane_n.y * p.y + plane_n.z * p.z - Vector_Dot(plane_n, plane_p));
-		};
-
-		// Create two temporary storage arrays to classify points either side of plane
-		// If distance sign is positive, point lies on "inside" of plane
-		vec3d* inside_points[3];  int nInsidePointCount = 0;
-		vec3d* outside_points[3]; int nOutsidePointCount = 0;
-		vec2d* inside_tex[3]; int nInsideTexCount = 0;
-		vec2d* outside_tex[3]; int nOutsideTexCount = 0;
-
-
-		// Get signed distance of each point in triangle to plane
-		float d0 = dist(in_tri.p[0]);
-		float d1 = dist(in_tri.p[1]);
-		float d2 = dist(in_tri.p[2]);
-
-		if (d0 >= 0) { inside_points[nInsidePointCount++] = &in_tri.p[0]; inside_tex[nInsideTexCount++] = &in_tri.t[0]; }
-		else
-		{
-			outside_points[nOutsidePointCount++] = &in_tri.p[0]; outside_tex[nOutsideTexCount++] = &in_tri.t[0];
-		}
-		if (d1 >= 0)
-		{
-			inside_points[nInsidePointCount++] = &in_tri.p[1]; inside_tex[nInsideTexCount++] = &in_tri.t[1];
-		}
-		else
-		{
-			outside_points[nOutsidePointCount++] = &in_tri.p[1];  outside_tex[nOutsideTexCount++] = &in_tri.t[1];
-		}
-		if (d2 >= 0)
-		{
-			inside_points[nInsidePointCount++] = &in_tri.p[2]; inside_tex[nInsideTexCount++] = &in_tri.t[2];
-		}
-		else
-		{
-			outside_points[nOutsidePointCount++] = &in_tri.p[2];  outside_tex[nOutsideTexCount++] = &in_tri.t[2];
-		}
-
-		// Now classify triangle points, and break the input triangle into 
-		// smaller output triangles if required. There are four possible
-		// outcomes...
-
-		if (nInsidePointCount == 0)
-		{
-			// All points lie on the outside of plane, so clip whole triangle
-			// It ceases to exist
-
-			return 0; // No returned triangles are valid
-		}
-
-		if (nInsidePointCount == 3)
-		{
-			// All points lie on the inside of plane, so do nothing
-			// and allow the triangle to simply pass through
-			out_tri1 = in_tri;
-
-			return 1; // Just the one returned original triangle is valid
-		}
-
-		if (nInsidePointCount == 1 && nOutsidePointCount == 2)
-		{
-			// Triangle should be clipped. As two points lie outside
-			// the plane, the triangle simply becomes a smaller triangle
-
-			// Copy appearance info to new triangle
-			out_tri1.col = in_tri.col;
-
-			// The inside point is valid, so keep that...
-			out_tri1.p[0] = *inside_points[0];
-			out_tri1.t[0] = *inside_tex[0];
-
-			// but the two new points are at the locations where the 
-			// original sides of the triangle (lines) intersect with the plane
-			float t;
-			out_tri1.p[1] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
-			out_tri1.t[1].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
-			out_tri1.t[1].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
-			out_tri1.t[1].w = t * (outside_tex[0]->w - inside_tex[0]->w) + inside_tex[0]->w;
-
-			out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[1], t);
-			out_tri1.t[2].u = t * (outside_tex[1]->u - inside_tex[0]->u) + inside_tex[0]->u;
-			out_tri1.t[2].v = t * (outside_tex[1]->v - inside_tex[0]->v) + inside_tex[0]->v;
-			out_tri1.t[2].w = t * (outside_tex[1]->w - inside_tex[0]->w) + inside_tex[0]->w;
-
-			return 1; // Return the newly formed single triangle
-		}
-
-		if (nInsidePointCount == 2 && nOutsidePointCount == 1)
-		{
-			// Triangle should be clipped. As two points lie inside the plane,
-			// the clipped triangle becomes a "quad". Fortunately, we can
-			// represent a quad with two new triangles
-
-			// Copy appearance info to new triangles
-			out_tri1.col = in_tri.col;
-
-			out_tri2.col = in_tri.col;
-
-			// The first triangle consists of the two inside points and a new
-			// point determined by the location where one side of the triangle
-			// intersects with the plane
-			out_tri1.p[0] = *inside_points[0];
-			out_tri1.p[1] = *inside_points[1];
-			out_tri1.t[0] = *inside_tex[0];
-			out_tri1.t[1] = *inside_tex[1];
-
-			float t;
-			out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
-			out_tri1.t[2].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
-			out_tri1.t[2].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
-			out_tri1.t[2].w = t * (outside_tex[0]->w - inside_tex[0]->w) + inside_tex[0]->w;
-
-			// The second triangle is composed of one of he inside points, a
-			// new point determined by the intersection of the other side of the 
-			// triangle and the plane, and the newly created point above
-			out_tri2.p[0] = *inside_points[1];
-			out_tri2.t[0] = *inside_tex[1];
-			out_tri2.p[1] = out_tri1.p[2];
-			out_tri2.t[1] = out_tri1.t[2];
-			out_tri2.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[1], *outside_points[0], t);
-			out_tri2.t[2].u = t * (outside_tex[0]->u - inside_tex[1]->u) + inside_tex[1]->u;
-			out_tri2.t[2].v = t * (outside_tex[0]->v - inside_tex[1]->v) + inside_tex[1]->v;
-			out_tri2.t[2].w = t * (outside_tex[0]->w - inside_tex[1]->w) + inside_tex[1]->w;
-			return 2; // Return two newly formed triangles which form a quad
-		}
-	}
-
 	olc::Pixel GetColour(float lum)
 	{
 		int col = (lum * 255.0f);
@@ -314,17 +178,17 @@ protected:
 			fYaw += 2.0f * fElapsedTime;
 
 
+		
 
 
-
-		Clear(olc::BLACK);
+		Clear(olc::WHITE);
 
 		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++)
 			pDepthBuffer[i] = 0.0f;
 
 
 		mat4x4 matRotZ, matRotX;
-		//fTheta += 1.0f * fElapsedTime;
+		fTheta += 1.0f * fElapsedTime;
 
 		matRotZ = Matrix_RotationZ(fTheta);
 		matRotX = Matrix_RotationX(fTheta);
@@ -333,7 +197,6 @@ protected:
 		matTrans = Matrix_Translation(0.0f, 0.0f, 10.0f);
 
 		mat4x4 matWorld;
-		matWorld = Matrix_Identity();	// Form World Matrix
 		matWorld = matRotZ * matRotX; // Transform by rotation
 		matWorld = matWorld * matTrans; // Transform by translation
 
@@ -353,7 +216,7 @@ protected:
 		// Draw triangles
 		for (auto tri : meshCube.tris)
 		{
-			triangle triProjected, triTransformed, triViewed;
+			triangle triTransformed, triViewed, triProjected;
 
 			triTransformed.p[0] = Matrix_MultiplyVector(matWorld, tri.p[0]);
 			triTransformed.p[1] = Matrix_MultiplyVector(matWorld, tri.p[1]);
@@ -394,7 +257,8 @@ protected:
 				triViewed.t[1] = triTransformed.t[1];
 				triViewed.t[2] = triTransformed.t[2];
 
-				// Clip
+				// Clip Viewed Triangle against near plane, this could form two additional
+				// additional triangles. 
 				int nClippedTriangles = 0;
 				triangle clipped[2];
 
@@ -451,14 +315,6 @@ protected:
 				}
 			}
 		}
-
-		// Sort triangles
-		sort(vecTrianglesToRaster.begin(), vecTrianglesToRaster.end(), [](triangle& t1, triangle& t2)
-			{
-				float z1 = (t1.p[0].z + t1.p[1].z + t1.p[2].z) / 3.0f;
-				float z2 = (t2.p[0].z + t2.p[1].z + t2.p[2].z) / 3.0f;
-				return z1 > z2;
-			});
 
 		for (auto& triToRaster : vecTrianglesToRaster)
 		{
@@ -705,7 +561,7 @@ protected:
 int main()
 {
 	Engine3D engine;
-	if (engine.Construct(256, 240, 4, 4))
+	if (engine.Construct(256*2, 240, 2, 2))
 		engine.Start();
 
 	return 0;
--- 3DEngine/CustomMath.h
+++ 3DEngine/CustomMath.h
@@ -5,6 +5,7 @@
 #include <math.h>
 using namespace std;
 
+
 struct mat4x4
 {
 
@@ -444,6 +445,8 @@ mat4x4 Matrix_Projection(float fFovDegrees, float fAspectRatio, float fNear, flo
 {
 	float fFovRad = 1.0f / tanf(fFovDegrees * 0.5f / 180.0f * 3.14159f);
 	mat4x4 matrix;
+	matrix.m[0][0] = 1;
+	matrix.m[1][1] = 1;
 	matrix.m[0][0] = fAspectRatio * fFovRad;
 	matrix.m[1][1] = fFovRad;
 	matrix.m[2][2] = fFar / (fFar - fNear);
@@ -488,4 +491,145 @@ mat4x4 Matrix_QuickInverse(mat4x4& m) // Only for Rotation/Translation Matrices
 	matrix.m[3][2] = -(m.m[3][0] * matrix.m[0][2] + m.m[3][1] * matrix.m[1][2] + m.m[3][2] * matrix.m[2][2]);
 	matrix.m[3][3] = 1.0f;
 	return matrix;
+}
+
+int Triangle_ClipAgainstPlane(vec3d plane_p, vec3d plane_n, triangle& in_tri, triangle& out_tri1, triangle& out_tri2)
+{
+	// Make sure plane normal is indeed normal
+	plane_n = plane_n.Normalise();
+
+	// Return signed shortest distance from point to plane, plane normal must be normalised
+	auto dist = [&](vec3d& p)
+	{
+		vec3d n = p.Normalise();
+		return (plane_n.x * p.x + plane_n.y * p.y + plane_n.z * p.z - Vector_Dot(plane_n, plane_p));
+	};
+
+	// Create two temporary storage arrays to classify points either side of plane
+	// If distance sign is positive, point lies on "inside" of plane
+	vec3d* inside_points[3];  int nInsidePointCount = 0;
+	vec3d* outside_points[3]; int nOutsidePointCount = 0;
+	vec2d* inside_tex[3]; int nInsideTexCount = 0;
+	vec2d* outside_tex[3]; int nOutsideTexCount = 0;
+
+
+	// Get signed distance of each point in triangle to plane
+	float d0 = dist(in_tri.p[0]);
+	float d1 = dist(in_tri.p[1]);
+	float d2 = dist(in_tri.p[2]);
+
+	if (d0 >= 0) 
+	{
+		inside_points[nInsidePointCount++] = &in_tri.p[0]; inside_tex[nInsideTexCount++] = &in_tri.t[0];
+	}
+	else
+	{
+		outside_points[nOutsidePointCount++] = &in_tri.p[0]; outside_tex[nOutsideTexCount++] = &in_tri.t[0];
+	}
+
+	if (d1 >= 0)
+	{
+		inside_points[nInsidePointCount++] = &in_tri.p[1]; inside_tex[nInsideTexCount++] = &in_tri.t[1];
+	}
+	else
+	{
+		outside_points[nOutsidePointCount++] = &in_tri.p[1];  outside_tex[nOutsideTexCount++] = &in_tri.t[1];
+	}
+
+	if (d2 >= 0)
+	{
+		inside_points[nInsidePointCount++] = &in_tri.p[2]; inside_tex[nInsideTexCount++] = &in_tri.t[2];
+	}
+	else
+	{
+		outside_points[nOutsidePointCount++] = &in_tri.p[2];  outside_tex[nOutsideTexCount++] = &in_tri.t[2];
+	}
+
+	// Now classify triangle points, and break the input triangle into 
+	// smaller output triangles if required. There are four possible
+	// outcomes...
+
+	if (nInsidePointCount == 0)
+	{
+		// All points lie on the outside of plane, so clip whole triangle
+		// It ceases to exist
+
+		return 0; // No returned triangles are valid
+	}
+
+	if (nInsidePointCount == 3)
+	{
+		// All points lie on the inside of plane, so do nothing
+		// and allow the triangle to simply pass through
+		out_tri1 = in_tri;
+
+		return 1; // Just the one returned original triangle is valid
+	}
+
+	if (nInsidePointCount == 1 && nOutsidePointCount == 2)
+	{
+		// Triangle should be clipped. As two points lie outside
+		// the plane, the triangle simply becomes a smaller triangle
+
+		// Copy appearance info to new triangle
+		out_tri1.col = in_tri.col;
+
+		// The inside point is valid, so keep that...
+		out_tri1.p[0] = *inside_points[0];
+		out_tri1.t[0] = *inside_tex[0];
+
+		// but the two new points are at the locations where the 
+		// original sides of the triangle (lines) intersect with the plane
+		float t;
+		out_tri1.p[1] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
+		out_tri1.t[1].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
+		out_tri1.t[1].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
+		out_tri1.t[1].w = t * (outside_tex[0]->w - inside_tex[0]->w) + inside_tex[0]->w;
+
+		out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[1], t);
+		out_tri1.t[2].u = t * (outside_tex[1]->u - inside_tex[0]->u) + inside_tex[0]->u;
+		out_tri1.t[2].v = t * (outside_tex[1]->v - inside_tex[0]->v) + inside_tex[0]->v;
+		out_tri1.t[2].w = t * (outside_tex[1]->w - inside_tex[0]->w) + inside_tex[0]->w;
+
+		return 1; // Return the newly formed single triangle
+	}
+
+	if (nInsidePointCount == 2 && nOutsidePointCount == 1)
+	{
+		// Triangle should be clipped. As two points lie inside the plane,
+		// the clipped triangle becomes a "quad". Fortunately, we can
+		// represent a quad with two new triangles
+
+		// Copy appearance info to new triangles
+		out_tri1.col = in_tri.col;
+
+		out_tri2.col = in_tri.col;
+
+		// The first triangle consists of the two inside points and a new
+		// point determined by the location where one side of the triangle
+		// intersects with the plane
+		out_tri1.p[0] = *inside_points[0];
+		out_tri1.p[1] = *inside_points[1];
+		out_tri1.t[0] = *inside_tex[0];
+		out_tri1.t[1] = *inside_tex[1];
+
+		float t;
+		out_tri1.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[0], *outside_points[0], t);
+		out_tri1.t[2].u = t * (outside_tex[0]->u - inside_tex[0]->u) + inside_tex[0]->u;
+		out_tri1.t[2].v = t * (outside_tex[0]->v - inside_tex[0]->v) + inside_tex[0]->v;
+		out_tri1.t[2].w = t * (outside_tex[0]->w - inside_tex[0]->w) + inside_tex[0]->w;
+
+		// The second triangle is composed of one of he inside points, a
+		// new point determined by the intersection of the other side of the 
+		// triangle and the plane, and the newly created point above
+		out_tri2.p[0] = *inside_points[1];
+		out_tri2.t[0] = *inside_tex[1];
+		out_tri2.p[1] = out_tri1.p[2];
+		out_tri2.t[1] = out_tri1.t[2];
+		out_tri2.p[2] = Vector_IntersectPlane(plane_p, plane_n, *inside_points[1], *outside_points[0], t);
+		out_tri2.t[2].u = t * (outside_tex[0]->u - inside_tex[1]->u) + inside_tex[1]->u;
+		out_tri2.t[2].v = t * (outside_tex[0]->v - inside_tex[1]->v) + inside_tex[1]->v;
+		out_tri2.t[2].w = t * (outside_tex[0]->w - inside_tex[1]->w) + inside_tex[1]->w;
+		return 2; // Return two newly formed triangles which form a quad
+	}
 }
